3c4d34d9735ca7e002c5637f77c9afae1ef3e2f0
rogue <roguefirework@github.com>
2025-04-16 10:48:53 -0700

    Partial neo4j c# integratoin

--- api/wumpapi/api/Program.cs
+++ api/wumpapi/api/Program.cs
@@ -1,4 +1,21 @@
+using Neo4j.Driver;
+using wumpapi.configuration;
+using wumpapi.neo4j;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<ApplicationSettings>(builder.Configuration.GetSection("ApplicationSettings"));
+var settings = new ApplicationSettings();
+builder.Configuration.GetSection("ApplicationSettings").Bind(settings);
+builder.Services.AddSingleton(GraphDatabase.Driver(settings.Neo4jConnection, AuthTokens.Basic(settings.Neo4jUser, settings.Neo4jPassword)));
+builder.Services.AddScoped<INeo4jDataAccess, Neo4jDataAccess>();
+builder.Services.AddTransient<IPersonRepository, PersonRepository>();
+
+
+
+
+
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -38,3 +55,5 @@ app.MapGet("/add", () =>
     }).WithName("/add");
 
 app.Run();
+
+
