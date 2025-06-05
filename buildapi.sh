e083dd479d0bc948364e39cc1f61010570f03125
rogue <roguefirework@github.com>
2025-06-05 08:58:29 -0700

    Extra tooling scheningans

--- buildapi.sh
+++ buildapi.sh
@@ -1,3 +1,4 @@
 # bin/bash
-(cd api/wumpapi ; docker build . -t wumpapi:latest ; docker images)
+(cd api/wumpapi ; docker build . -t wumpapi:latest; docker images)
+docker image tag wumpapi:latest roguefirework/wumpapi:latest
 (cd dockercontainers ; docker compose stop ; docker compose up --force-recreate -d)
